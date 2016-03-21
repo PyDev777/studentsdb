78d3c452660c5d086ad37e4042dad1b69c9b8945
PyDev777 <pydev@ukr.net>
2016-03-21 22:54:20 +0200

    add students_list.html to template

--- students/views.py
+++ students/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+
 
 # Create your views here.
+
+def students_list1(request):
+    return HttpResponse('<h1>Hello World!</h1>')
+
+def students_list2(request, sid):
+    try:
+        sid = int(sid)
+    except ValueError:
+        raise Http404
+    else:
+        return HttpResponse('<h1>Hello World!</h1>')
+
+def students_list3(request):
+    template = loader.get_template('demo.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
+
+
+# Views for Students
+
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+def students_add(request):
+    return HttpResponse('<h1>Student Add Form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+
+# Views for Groups
+
+def groups_list(request):
+    return HttpResponse('<h1>Groups Listing</h1>')
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
+
+
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -6,5 +6,18 @@ urlpatterns = patterns('',
     # url(r'^$', 'studentsdb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # Students urls
+    url(r'^$', 'students.views.students_list', name='home'),
+    url(r'^students/add/$', 'students.views.students_add', name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', 'students.views.students_edit', name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', 'students.views.students_delete', name='students_delete'),
+
+    # Groups urls
+    url(r'^groups/$', 'students.views.groups_list', name='groups'),
+    url(r'^groups/add/$', 'students.views.groups_add', name='groups_add'),
+    url(r'^groups/(?P<sid>\d+)/edit/$', 'students.views.groups_edit', name='groups_edit'),
+    url(r'^groups/(?P<sid>\d+)/delete/$', 'students.views.groups_delete', name='groups_delete'),
+
+
     url(r'^admin/', include(admin.site.urls)),
 )
