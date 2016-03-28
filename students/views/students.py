b74909a74ae46cd5734a8670adef5176c982cc22
PyDev777 <pydev@ukr.net>
2016-03-29 02:45:46 +0300

    Students list in students template

--- students/views/students.py
+++ students/views/students.py
@@ -3,28 +3,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
+from ..models import Student
 
 
 # Views for Students
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Андрій',
-         'last_name': u'Корост',
-         'ticket': 2123,
-         'image': 'img/podoba3.jpg'},
-        {'id': 2,
-         'first_name': u'Віталій',
-         'last_name': u'Подоба',
-         'ticket': 254,
-         'image': 'img/me.jpeg'},
-        {'id': 3,
-         'first_name': u'Тарас',
-         'last_name': u'Притула',
-         'ticket': 5332,
-         'image': 'img/piv.png'}
-    )
+    students = Student.objects.all()
     return render(request, 'students/students_list.html', {'students': students, 'students_url': reverse('home')})
 
 
