36e4aaeae119a926ae49323d670e5c645eadc054
PyDev777 <pydev@ukr.net>
2016-03-30 01:25:01 +0300

    default user image

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .settings import MEDIA_ROOT, DEBUG
 
 urlpatterns = patterns('',
     # Examples:
@@ -24,3 +25,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': MEDIA_ROOT}))
