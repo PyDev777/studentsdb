ce2fa7315b88e1a5a66ec2c272f03797c21daabd
PyDev777 <pydev@ukr.net>
2016-03-30 04:20:47 +0300

    students order_by & reverse

--- students/views/students.py
+++ students/views/students.py
@@ -10,6 +10,13 @@ from ..models import Student
 
 def students_list(request):
     students = Student.objects.all()
+
+    # try to order students list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('last_name', 'first_name', 'ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
     return render(request, 'students/students_list.html', {'students': students, 'students_url': reverse('home')})
 
 
