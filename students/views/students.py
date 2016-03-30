8e063f5cf727174dfd64b7de1e92eb7ed6312a89
PyDev777 <pydev@ukr.net>
2016-03-30 05:14:24 +0300

    students order_by  id

--- students/views/students.py
+++ students/views/students.py
@@ -13,7 +13,7 @@ def students_list(request):
 
     # try to order students list
     order_by = request.GET.get('order_by', '')
-    if order_by in ('last_name', 'first_name', 'ticket'):
+    if order_by in ('id', 'last_name', 'first_name', 'ticket'):
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
