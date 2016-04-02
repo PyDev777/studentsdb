8f60e30c2a634f9cb816267db8772f20d4bf0918
PyDev777 <pydev@ukr.net>
2016-04-02 18:08:49 +0300

    add group models

--- students/models.py
+++ students/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 
+
 # Create your models here.
 
 class Student(models.Model):
@@ -14,44 +15,67 @@ class Student(models.Model):
     first_name = models.CharField(
         max_length=256,
         blank=False,
-        verbose_name=u"Ім'я"
-    )
+        verbose_name=u"Ім'я")
 
     last_name = models.CharField(
         max_length=256,
         blank=False,
-        verbose_name=u"Прізвище"
-    )
+        verbose_name=u"Прізвище")
 
     middle_name = models.CharField(
         max_length=256,
         blank=True,
         verbose_name=u"По-батькові",
-        default=""
-    )
+        default="")
 
     birthday = models.DateField(
         blank=False,
         verbose_name=u"Дата народження",
-        null=True
-    )
+        null=True)
 
     photo = models.ImageField(
         blank=True,
         verbose_name=u"Фото",
-        null=True
-    )
+        null=True)
 
     ticket = models.CharField(
         max_length=256,
         blank=False,
-        verbose_name=u"Білет"
-    )
+        verbose_name=u"Білет")
 
     notes = models.TextField(
         blank=True,
-        verbose_name=u"Додаткові нотатки"
-    )
+        verbose_name=u"Додаткові нотатки")
 
     def __unicode__(self):
         return u"%s %s" % (self.first_name, self.last_name)
+
+
+class Group(models.Model):
+    """Group model"""
+
+    class Meta(object):
+        verbose_name = u'Група'
+        verbose_name_plural = u'Групи'
+
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Назва")
+
+    leader = models.OneToOneField(
+        'Student',
+        null=True,
+        blank=True,
+        verbose_name=u"Староста",
+        on_delete=models.SET_NULL)
+
+    notes = models.TextField(
+        blank=True,
+        verbose_name=u"Додаткові нотатки")
+
+    def __unicode__(self):
+        if self.leader:
+            return u"%s (%s %s)" % (self.title, self.leader.first_name, self.leader.last_name)
+        else:
+            return u"%s" % self.title
