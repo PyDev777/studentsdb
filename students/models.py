10f873f08bfe9fd96e6dd27d0997677c7ce9c029
PyDev777 <pydev@ukr.net>
2016-04-02 20:00:41 +0300

    add field student_group to Student model

--- students/models.py
+++ students/models.py
@@ -50,7 +50,7 @@ class Student(models.Model):
     student_group = models.ForeignKey(
         'Group',
         verbose_name=u"Група",
-        blank=False,
+        blank=True,
         null=True,
         on_delete=models.PROTECT)
 
