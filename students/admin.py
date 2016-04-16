4b67f12df9db27f01694cd95b57e503f0352e71a
PyDev777 <pydev@ukr.net>
2016-04-16 21:00:32 +0300

    StudentAdmin, GroupAdmin (without validation)

--- students/admin.py
+++ students/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
-from .models import Student
-from .models import Group
+from django.core.urlresolvers import reverse
+from .models import Student, Group
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 4
+    search_fields = ['last_name', 'first_name', 'ticket', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs={'pk': obj.id})
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+    list_display_links = ['title']
+    list_editable = ['leader']
+    ordering = ['title']
+    list_filter = ['leader']
+    list_per_page = 2
+    search_fields = ['title', 'leader', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('groups_edit', kwargs={'pk': obj.id})
+
 
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Group)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group, GroupAdmin)
+
 
