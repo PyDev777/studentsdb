3710276b1bd8dd74b3b9a2094569af7b59b18205
PyDev777 <pydev@ukr.net>
2016-06-27 17:08:49 +0300

    logging to file (Students add&update)

--- students/signals.py
+++ students/signals.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -6,4 +9,10 @@ from .models import Student
 
 @receiver(post_save, sender=Student)
 def log_student_updated_added_event(sender, **kwargs):
-    print sender
+    """Writes information about newly added or updated student into log file"""
+    logger = logging.getLogger(__name__)
+    student = kwargs['instance']
+    if kwargs['created']:
+        logger.info("Student added: %s %s (ID: %d)", student.first_name, student.last_name, student.id)
+    else:
+        logger.info("Student updated: %s %s (ID: %d)", student.first_name, student.last_name, student.id)
