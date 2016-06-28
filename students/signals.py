61d27daf4625f68abb6e0f0ce9a1da6dbfb9e201
PyDev777 <pydev@ukr.net>
2016-06-28 03:41:53 +0300

    logging to file (Students&Groups add&update&delete)

--- students/signals.py
+++ students/signals.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 import logging
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import Student
+from .models import Student, Group
 
 
 @receiver(post_save, sender=Student)
@@ -16,3 +16,30 @@ def log_student_updated_added_event(sender, **kwargs):
         logger.info("Student added: %s %s (ID: %d)", student.first_name, student.last_name, student.id)
     else:
         logger.info("Student updated: %s %s (ID: %d)", student.first_name, student.last_name, student.id)
+
+
+@receiver(post_delete, sender=Student)
+def log_student_deleted_event(sender, **kwargs):
+    """Writes information about newly deleted student into log file"""
+    logger = logging.getLogger(__name__)
+    student = kwargs['instance']
+    logger.info("Student deleted: %s %s (ID: %d)", student.first_name, student.last_name, student.id)
+
+
+@receiver(post_save, sender=Group)
+def log_group_updated_added_event(sender, **kwargs):
+    """Writes information about newly added or updated group into log file"""
+    logger = logging.getLogger(__name__)
+    group = kwargs['instance']
+    if kwargs['created']:
+        logger.info("Group added: %s (ID: %d)", group.title, group.id)
+    else:
+        logger.info("Group updated: %s (ID: %d)", group.title, group.id)
+
+
+@receiver(post_delete, sender=Group)
+def log_group_deleted_event(sender, **kwargs):
+    """Writes information about newly deleted group into log file"""
+    logger = logging.getLogger(__name__)
+    group = kwargs['instance']
+    logger.info("Group deleted: %s (ID: %d)", group.title, group.id)
