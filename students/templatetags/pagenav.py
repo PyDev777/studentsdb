caea2439a315a61ce31b96b8bfb336aa1a54a7ed
PyDev777 <pydev@ukr.net>
2016-08-18 03:43:16 +0300

    Inclusion_tag {% pagenav %} to templatetags

--- students/templatetags/pagenav.py
+++ students/templatetags/pagenav.py
@@ -1,20 +1,17 @@
 from django import template
-from django.template.loader import get_template
-
 
 register = template.Library()
 
 
 # Usage: {% pagenav object_list=students base_url=students_url order_by=order_by reverse=reverse cur_month=cur_month is_paginated=is_paginated paginator=paginator %}
 
-@register.simple_tag
-def pagenav(*args, **kwargs):
-    t = get_template('students/pagination.html')
-    return t.render(template.Context({
-        'object_list': kwargs['object_list'],
-        'base_url': kwargs['base_url'],
-        'order_by': kwargs['order_by'],
-        'reverse': kwargs['reverse'],
-        'cur_month': kwargs['cur_month'],
-        'is_paginated': kwargs['is_paginated'],
-        'paginator': kwargs['paginator']}))
+@register.inclusion_tag('students/pagination.html')
+def pagenav(object_list, base_url, order_by, reverse, cur_month, is_paginated, paginator):
+    """Display page navigation for given list of objects"""
+    return {'object_list': object_list,
+            'base_url': base_url,
+            'order_by': order_by,
+            'reverse': reverse,
+            'cur_month': cur_month,
+            'is_paginated': is_paginated,
+            'paginator': paginator}
