2a28d2cab0d4908463832d4ea564853bfe0c500b
PyDev777 <pydev@ukr.net>
2016-08-21 18:25:00 +0300

    test_student_forms

--- students/tests/test_student_forms.py
+++ students/tests/test_student_forms.py
@@ -40,7 +40,7 @@ class TestStudentUpdateForm(TestCase):
 
         # post form with valid data
         # TODO: post new photo for homework
-        group = Group.objects.filter(title='Group2')[0]
+        group = Group.objects.filter(title='Group1')[0]
         response = self.client.post(self.url, {
             'first_name': 'Updated Name', 'last_name': 'Updated Last Name', 'ticket': '567', 'student_group': group.id, 'birthday': '1990-11-11'
         }, follow=True)
@@ -59,46 +59,60 @@ class TestStudentUpdateForm(TestCase):
         self.assertIn('Student updated successfully', response.content)
         self.assertEqual(response.redirect_chain[0][0], 'http://testserver/?status_message=' + 'Student%20updated%20successfully!')
 
-    # def test_cancel(self):
-    #     # login as admin to access student edit form
-    #     self.client.login(username='admin', password='admin')
-    #
-    #     # post form with Cancel button
-    #     response = self.client.post(self.url, {'cancel_button': 'Cancel'},
-    #         follow=True)
-    #
-    #     self.assertIn('Student update canceled!', response.content)
-    #     self.assertEqual(response.redirect_chain[0][0],
-    #         'http://testserver/?status_message=' +
-    #         'Student%20update%20canceled!')
+    def test_fail_leader(self):
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
 
-    # def test_access(self):
-    #     # try to access form as anonymous user
-    #     response = self.client.get(self.url, follow=True)
-    #
-    #     # we have to get 200 code and login form
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #     # check that we're on login form
-    #     self.assertIn('Login Form', response.content)
-    #
-    #     # check redirect url
-    #     self.assertEqual(response.redirect_chain[0], ('http://testserver/users/login/?next=/students/1/edit/', 302))
+        # post form with valid data
+        # TODO: post new photo for homework
+        group = Group.objects.filter(title='Group2')[0]
+        response = self.client.post(self.url, {
+            'first_name': 'Updated Name', 'last_name': 'Updated Last Name', 'ticket': '567', 'student_group': group.id, 'birthday': '1990-11-11'
+        }, follow=True)
 
-    # def test_styles(self):
-    #     # login as admin to access student edit form
-    #     self.client.login(username='admin', password='admin')
-    #
-    #     # get form and check few fields there
-    #     response = self.client.get(self.url)
-    #
-    #     # check response status
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #     # check classes
-    #     self.assertIn('form-horizontal', response.content)
-    #     self.assertIn('form-group', response.content)
-    #     self.assertIn('control-label', response.content)
-    #     self.assertIn('controls', response.content)
-    #     self.assertIn('form-actions', response.content)
-    #     self.assertIn('btn-primary', response.content)
+        # check response status
+        self.assertEqual(response.status_code, 200)
+
+        # check proper redirect after form post
+        self.assertIn('Student is the leader of another group!', response.content)
+
+    def test_cancel(self):
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
+
+        # post form with Cancel button
+        response = self.client.post(self.url, {'cancel_button': 'Cancel'}, follow=True)
+
+        self.assertIn('Student update canceled!', response.content)
+        self.assertEqual(response.redirect_chain[0][0], 'http://testserver/?status_message=' + 'Student%20update%20canceled!')
+
+    def test_access(self):
+        # try to access form as anonymous user
+        response = self.client.get(self.url, follow=True)
+
+        # we have to get 200 code and login form
+        self.assertEqual(response.status_code, 200)
+
+        # check that we're on login form
+        self.assertIn('Login Form', response.content)
+
+        # check redirect url
+        self.assertEqual(response.redirect_chain[0], ('http://testserver/users/login/?next=/students/1/edit/', 302))
+
+    def test_styles(self):
+        # login as admin to access student edit form
+        self.client.login(username='admin', password='admin')
+
+        # get form and check few fields there
+        response = self.client.get(self.url)
+
+        # check response status
+        self.assertEqual(response.status_code, 200)
+
+        # check classes
+        # self.assertIn('form-horizontal', response.content)
+        self.assertIn('form-group', response.content)
+        # self.assertIn('control-label', response.content)
+        # self.assertIn('controls', response.content)
+        # self.assertIn('form-actions', response.content)
+        self.assertIn('btn-primary', response.content)
